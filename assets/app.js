8bf14f3c3ffad22193652dc7f5014ab2a183cb0e
Nathan <sabinnat001@gmail.com>
2023-04-08 14:37:07 -0700

    Added JS,HTML,CSS

---
+++ assets/app.js
@@ -0,0 +1,16 @@
+// render in welcome screen by inseting an h1 tag for a tittle and a start button
+function init(){
+    
+}
+
+// event lisener to check to see if the start button was pressed
+// function body to render in first question and start a timer function
+    // each question will have 4 buttons under 3 of which will have a class of wrong, one will have a correct function
+    // the buttons will all have an event listner to move on to the next question these 
+    // the buttons will also add to a total ammount correct function 
+
+// timmer function this function will start a timer that will be displayed in the render question function 
+    // if timer = 0 stop quiz call highscore function
+
+// Highscore function this function will handle storing the ammount correct then ask for a name and finally render in the highscore with names
+    // this function will also have a button to return to the render function
