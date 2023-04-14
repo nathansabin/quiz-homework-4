0b8138bcf843e641971cd1f6a976a472c63f2ab8
Nathan <sabinnat001@gmail.com>
2023-04-13 21:25:57 -0700

    Fixed bugs

--- assets/app.js
+++ assets/app.js
@@ -1,11 +1,15 @@
-// setting varriables
-var correct; 
+//TODO
+    // make the correct amount add to the database
+    // fix next question
 
+// setting varriables
+var amountCorrect; 
+
+var timerInterval;
 var timerSeconds = 30;
 var timer;
 
 var text;
-var start;
 var questions;
 var answer = [];
 var quizAnswers = [];
@@ -20,40 +24,55 @@ text = document.createElement("h2");
 
 // Start will render in the start button and and a short discription.
 function start(){
-    correct = 0;
-    name = "";
+    amountCorrect = 0;
+    timerSeconds = 30;
     text.textContent = "Press the button bellow to begin the quiz";
     startButton = document.createElement("h2");
     startButton.textContent = "Start";
     startButton.setAttribute("class", "start-button");
+    startButton.addEventListener("click", quizOne);
 
     document.body.append(text);
     document.body.append(startButton);
 }
 
 // handles event listner for all of the question buttons
-function nextQuestion(nextSlide){
-    for (var i = 0; i < answer.length; i++) {
+function nextQuestion(nextSlide, index){
+    for (var i = 0; i < quizAnswers.length; i++) {
         questions.children[i].addEventListener("click", function(event) {
-          var selectedAnswer = event.target;
-          if (selectedAnswer.getAttribute("id") === "correct"){
-            correct++;
-          }
-          else {
-            timerSeconds = timerSeconds - 10;
-          }
-          nextSlide();
-});  
-}
+        selectedAnswer = event.target;
+
+        // if (selectedAnswer.getAttribute("id") === "correct"){
+        //     amountCorrect++;
+        //     nextSlide()
+        //     }
+        //     else {
+        //         timerSeconds = timerSeconds - 5;
+        //         nextSlide()
+        //         }
+        // })}
+        
+        if (selectedAnswer == questions.children[index]){
+            amountCorrect++;
+            nextSlide()
+            }
+            else {
+                timerSeconds = timerSeconds - 5;
+                nextSlide()
+                }
+        })}
+
+            
+
 }
 
 // timmerCount will start a timer and added it to the botom right of the screen then check if it is equal to 0. If it is equal to 0 then it will take you to the leader board.
 function timerCount(){
-     var timerInterval = setInterval(function () {
+     timerInterval = setInterval(function () {
         timer.textContent = "Timer:" + timerSeconds;
         timerSeconds--;
 
-        if (timerSeconds === 0){
+        if (timerSeconds <= 0){
             scoreboard();
         }
     }, 1000);
@@ -61,7 +80,6 @@ function timerCount(){
 
 // Quiz will render in the qustions, start the timer, keep track of how many are correct. if all question are completed then it will load in the scoreboard function
 function quizOne(){
-    correct = 0;
     timer = document.createElement("h3");
     timer.setAttribute("class", "timer");
     document.body.append(timer);
@@ -77,7 +95,7 @@ function quizOne(){
         questions.appendChild(answer[i]);
     }
 
-    answer[3].setAttribute("id", "correct");
+    //answer[3].setAttribute("id", "correct");
     document.body.append(questions);
     for (var i = 0; i < quizAnswers.length; i++){
         questions.appendChild(answer[i]);
@@ -85,7 +103,7 @@ function quizOne(){
 
     text.textContent = "what is an ASCII value?";
 
-    nextQuestion(quizTwo);
+    nextQuestion(quizTwo, 3);
 }
 
 // Updates question and the id correct
@@ -98,8 +116,8 @@ function quizTwo(){
         answer[i].textContent = quizAnswers[i];
     }
     
-    answer[2].setAttribute("id", "correct")
-    nextQuestion(quizThree);
+    // answer[2].setAttribute("id", "correct")
+    nextQuestion(quizThree, 2);
 }   
 
 // Updates question and the id correct
@@ -112,51 +130,53 @@ function quizThree(){
         answer[i].textContent = quizAnswers[i];
     }
     
-    answer[0].setAttribute("id", "correct")
-    nextQuestion(scoreboard);
+    //answer[0].setAttribute("id", "correct")
+    nextQuestion(scoreboard, 0);
 }
-
-//TODO
-    // make the correct amount add to the database
-    // clear database
-    // make it so you go back to start if you do not enter intials
-    // Make it so it doesnt promt for a name twice
 
 // Highscore function this function will handle storing the ammount correct then ask for a name and finally render in the highscore with names
 function scoreboard(){
     questions.remove();
     timer.remove();
     text.textContent = "Score Board";
-    clearInterval(timerSeconds);
-
+    clearInterval(timerInterval);
    
     name = prompt("What are your intials?");
-    scores = JSON.parse(localStorage.getItem("scores")) || [];
-    scores.push({name: name, score: correct});
-    localStorage.setItem("scores", JSON.stringify(scores));
+    if (name !== "null"){
+        scores = JSON.parse(localStorage.getItem("scores")) || [];
+        scores.push({name: name, score: amountCorrect});
+        localStorage.setItem("scores", JSON.stringify(scores));
 
-    var scoreboard = document.createElement("ul");
-    scoreboard.setAttribute("class", "score-board");
-    document.body.append(scoreboard);
+        scoreboard = document.createElement("ul");
+        scoreboard.setAttribute("class", "score-board");
+        document.body.append(scoreboard);
 
-    for (var i = 0; i < scores.length; i++){
-        var scoreEntry = document.createElement("li");
-        scoreEntry.setAttribute("class", "score-box");
-        scoreEntry.textContent = scores[i].name + ": " + scores[i].score;
-        scoreboard.appendChild(scoreEntry);
+        for (var i = 0; i < scores.length; i++){
+            var scoreEntry = document.createElement("li");
+            scoreEntry.setAttribute("class", "score-box");
+            scoreEntry.textContent = scores[i].name + ": " + scores[i].score;
+            scoreboard.appendChild(scoreEntry);
+        }
+
+        startButton = document.createElement("h2");
+        startButton.textContent = "Start";
+        startButton.setAttribute("class", "start-button");
+        document.body.append(startButton);
+
+        startButton.addEventListener("click", reset);
+    }
+    else{
+        start();
+    }
     }
 
-    startButton = document.createElement("h2");
-    startButton.textContent = "Try again";
-    startButton.setAttribute("class", "start-button");
+// This function will remove the scoreboard and reset the data so it can be grabbed again, then it will call start again
+function reset(){
+    //scoreboard.remove();
+    //startButton.remove();
+   location.reload();
+    //start();
+    //localStorage.clear();
+    }
 
-    startbutton.addEventListener("click", start());
-}
-
-// This function will put all of the other functions together, and handle all event listeners
-function init(){
-    start();
-    startButton.addEventListener("click", quizOne);
-}
-
-init()
+start()
